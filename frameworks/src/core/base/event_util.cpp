ed523afb317b21b8bcdfb010ec199bcde112df4b
youzhi92 <chenyouzhi@huawei.com>
2021-10-13 10:56:34 +0800

    fixed bcbf6cc from https://gitee.com/youzhi92/ace_engine_lite/pulls/407 issueNo: I4DNDZ Description: fix the bug of failure probability of triggering click event Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: youzhi92 <chenyouzhi@huawei.com>
    Change-Id: I5de3179f34f8bd49ae2b8aef8b6f5c5ed42d5afb

--- frameworks/src/core/base/event_util.cpp
+++ frameworks/src/core/base/event_util.cpp
@@ -52,6 +52,7 @@ void CallbackExecutor(void *data)
     JSValue args[argsLength] = {params->arg};
     JSRelease(JSFunction::Call(params->fn, params->vm, args, argsLength));
     JSRelease(params->arg);
+    JSRelease(params->vm);
     delete params;
     params = nullptr;
 }
@@ -126,6 +127,7 @@ void EventUtil::InvokeCallback(JSValue vm, JSValue callback, JSValue event, cons
         HILOG_ERROR(HILOG_MODULE_ACE, "EventUtil::InvokeCallback failed: Async task dispatch failure.");
         delete params;
         params = nullptr;
+        JSRelease(vm);
         JSRelease(event);
     }
 }
