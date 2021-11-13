esp-arkui/arkui_ace_engine_lite

Description: support lite media query IssueNo: I4DSDQ Feature or Bugfix: Feature Binary Source:No
Signed-off-by: jiadexiang jiadexiang@huawei.com

frameworks/src/core/stylemgr/app_style.cpp
@@ -326,5 +326,28 @@ AppStyle *AppStyle::GenerateFromJS(jerry_value_t styleKey, jerry_value_t styleVa
 
     return newStyle;
 }
+
+void AppStyle::CombinStyles(AppStyle &dest, const AppStyle &source, bool overwrite)
+{
+    const AppStyleItem *styleItem = source.GetFirst();
+    while (styleItem != nullptr) {
+        const AppStyleItem *currentStyleItemInSource = styleItem;
+        styleItem = styleItem->GetNext();
+        const AppStyleItem *existOneInDest = dest.GetStyleItemByNameId(currentStyleItemInSource->GetPropNameId());
+
+        if (existOneInDest == nullptr) {
+            dest.AddStyleItem(AppStyleItem::CopyFrom(currentStyleItemInSource));
+            continue;
+        }
+
+        if (!overwrite) {
+            // exist one has higher priority
+            continue;
+        }
+
+        // the new one has higher priority, to over wirte exist one
+        const_cast<AppStyleItem *>(existOneInDest)->UpdateValueFrom(*currentStyleItemInSource);
+    }
+}
 } // namespace ACELite
 } // namespace OHOS

frameworks/src/core/stylemgr/app_style.h
@@ -33,7 +33,7 @@ public:
         Reset();
     }
 
-    const char* GetStyleName()
+    const char* GetStyleName() const
     {
         return styleName_;
     }
@@ -65,7 +65,7 @@ public:
         next_ = nextStyle;
     }
 
-    const AppStyle* GetNext()
+    const AppStyle* GetNext() const
     {
         return next_;
     }
@@ -75,6 +75,7 @@ public:
     const AppStyleItem* GetStyleItemByName(const char * const stylePropName) const;
     const AppStyleItem* GetStyleItemByNameId(uint16_t stylePropNameId) const;
     static AppStyle* GenerateFromJS(jerry_value_t styleKey, jerry_value_t styleValue, bool isKeyFrames);
+    static void CombinStyles(AppStyle &dest, const AppStyle &source, bool overwrite = false);
 
 private:
     void SetStyleName(const char * const name, size_t nameLen);

frameworks/src/core/stylemgr/app_style_sheet.cpp
@@ -14,6 +14,9 @@
  */
 
 #include "stylemgr/app_style_sheet.h"
+#include "condition_arbitrator.h"
+#include "js_fwk_common.h"
+#include "scope_js_value.h"
 
 namespace OHOS {
 namespace ACELite {
@@ -44,15 +47,84 @@ void AppStyleSheet::InitSheet(jerry_value_t styleSheetObj)
         return;
     }
 
+    // clear previous style sheet list
     Reset();
 
+    // initializa all media query selectors first, as all media query selectors have higher priority
+    InitMediaSelectors(styleSheetObj);
+
+    // initialize all other normal selectors
+    InitNormalSelectors(styleSheetObj);
+}
+
+void AppStyleSheet::InitMediaSelectors(const jerry_value_t styleSheetObj)
+{
+    // check if media query exists in style sheet
+    const char * const mediaQueryArrayKeyName = "@media";
+    jerry_value_t propNameV =
+            jerry_create_string(reinterpret_cast<jerry_char_t *>(const_cast<char *>(mediaQueryArrayKeyName)));
+    ScopeJSValue propNameP(propNameV);
+    if (!HasOwnProperty(styleSheetObj, propNameV)) {
+        // no media querry items
+        return;
+    }
+
+    ScopeJSValue mediaQueryArrayValue(jerry_get_property(styleSheetObj, propNameV));
+    if (!jerry_value_is_array(mediaQueryArrayValue.Obain())) {
+        return;
+    }
+
+    uint32_t conditionCounts = jerry_get_array_length(mediaQueryArrayValue.Obain());
+    const uint32_t maxConditionCount = 256;
+    if (conditionCounts == 0 || conditionCounts > maxConditionCount) {
+        return;
+    }
+    for (uint32_t index = 0; index < conditionCounts; index++) {
+        // get the array item @ index position
+        jerry_value_t mediaArrayItemV = jerry_get_property_by_index(mediaQueryArrayValue.Obain(), index);
+        ScopeJSValue mediaArrayItemP(mediaArrayItemV);
+        // handle single one
+        HandleSingleMediaItem(mediaArrayItemV);
+    }
+}
+
+void AppStyleSheet::HandleSingleMediaItem(const jerry_value_t mediaItem)
+{
+    const char * const conditionKeyName = "condition";
+    jerry_value_t conditionKeyV = jerry_create_string(reinterpret_cast<const jerry_char_t *>(conditionKeyName));
+    ScopeJSValue conditionKeyP(conditionKeyV);
+    if (!JerryHasProperty(mediaItem, conditionKeyV)) {
+        return;
+    }
+    // get condition string
+    jerry_value_t conditionStrJSValue = jerry_get_property(mediaItem, conditionKeyV);
+    ScopeJSValue autoReleaseV(conditionStrJSValue);
+    char *conditionStr = MallocStringOf(conditionStrJSValue);
+    if (conditionStr == nullptr) {
+        return;
+    }
+    ConditionArbitrator arbitrator;
+    bool isMatched = arbitrator.Decide(conditionStr);
+    ace_free(conditionStr);
+    conditionStr = nullptr;
+    if (!isMatched) {
+        // the current media item is not matching with current device environment
+        return;
+    }
+
+    // matched, parse the item through normal way
+    InitNormalSelectors(mediaItem, true);
+}
+
+void AppStyleSheet::InitNormalSelectors(const jerry_value_t styleSheetObj, bool overwrite)
+{
     // init all id selectors
     const char * const attrIdSelectors = "idSelectors";
     jerry_value_t propName = jerry_create_string(reinterpret_cast<jerry_char_t *>(const_cast<char *>(attrIdSelectors)));
     jerry_value_t propValue = UNDEFINED;
     if (HasOwnProperty(styleSheetObj, propName)) {
         propValue = jerry_get_property(styleSheetObj, propName);
-        InitSelectors(&idSelectors_, propValue, false);
+        InitSelectors(&idSelectors_, propValue, false, overwrite);
         jerry_release_value(propValue);
     }
     jerry_release_value(propName);
@@ -62,7 +134,7 @@ void AppStyleSheet::InitSheet(jerry_value_t styleSheetObj)
     propName = jerry_create_string(reinterpret_cast<jerry_char_t *>(const_cast<char *>(attrClassSelectors)));
     if (HasOwnProperty(styleSheetObj, propName)) {
         propValue = jerry_get_property(styleSheetObj, propName);
-        InitSelectors(&classSelectors_, propValue, false);
+        InitSelectors(&classSelectors_, propValue, false, overwrite);
         jerry_release_value(propValue);
     }
     jerry_release_value(propName);
@@ -72,13 +144,14 @@ void AppStyleSheet::InitSheet(jerry_value_t styleSheetObj)
     propName = jerry_create_string(reinterpret_cast<jerry_char_t *>(const_cast<char *>(keyFrames)));
     if (HasOwnProperty(styleSheetObj, propName)) {
         propValue = jerry_get_property(styleSheetObj, propName);
-        InitSelectors(&keyFrameSelectors_, propValue, true);
+        InitSelectors(&keyFrameSelectors_, propValue, true, overwrite);
         jerry_release_value(propValue);
     }
     jerry_release_value(propName);
 }
 
-void AppStyleSheet::InitSelectors(AppStyleList** selectorsList, jerry_value_t selectorsObj, bool isKeyFrames)
+void AppStyleSheet::InitSelectors(AppStyleList **selectorsList, jerry_value_t selectorsObj,
+                                  bool isKeyFrames, bool overwrite)
 {
     if (jerry_value_is_undefined(selectorsObj)) {
         return;
@@ -98,9 +171,16 @@ void AppStyleSheet::InitSelectors(AppStyleList** selectorsList, jerry_value_t se
         /* convert style key into char */
         jerry_value_t styleKey = jerry_get_property_by_index(styleKeys, index);
         jerry_value_t styleValue = jerry_get_property(selectorsObj, styleKey);
-        AppStyle* newStyle = AppStyle::GenerateFromJS(styleKey, styleValue, isKeyFrames);
+        AppStyle *newStyle = AppStyle::GenerateFromJS(styleKey, styleValue, isKeyFrames);
         if (newStyle != nullptr) {
-            (*selectorsList)->AddStyle(newStyle);
+            AppStyle *existOne = (*selectorsList)->GetExistStyle(newStyle->GetStyleName());
+            if (existOne != nullptr) {
+                AppStyle::CombinStyles(*existOne, *newStyle, overwrite);
+                delete newStyle; // combined to exist one, no used
+                newStyle = nullptr;
+            } else {
+                (*selectorsList)->AddStyle(newStyle);
+            }
         }
         ReleaseJerryValue(styleKey, styleValue, VA_ARG_END_FLAG);
     }
@@ -108,28 +188,28 @@ void AppStyleSheet::InitSelectors(AppStyleList** selectorsList, jerry_value_t se
     jerry_release_value(styleKeys);
 }
 
-AppStyle* AppStyleSheet::GetStyleFromIDSelectors(const char * const name) const
+AppStyle *AppStyleSheet::GetStyleFromIDSelectors(const char * const name) const
 {
     return GetStyleFromSelectors(idSelectors_, name);
 }
 
-AppStyle* AppStyleSheet::GetStyleFromClassSelectors(const char * const name) const
+AppStyle *AppStyleSheet::GetStyleFromClassSelectors(const char * const name) const
 {
     return GetStyleFromSelectors(classSelectors_, name);
 }
 
-AppStyle* AppStyleSheet::GetStyleFromKeyFramesSelectors(const char * const name) const
+AppStyle *AppStyleSheet::GetStyleFromKeyFramesSelectors(const char * const name) const
 {
     return GetStyleFromSelectors(keyFrameSelectors_, name);
 }
 
-AppStyle* AppStyleSheet::GetStyleFromSelectors(AppStyleList* selectors, const char * const name) const
+AppStyle *AppStyleSheet::GetStyleFromSelectors(AppStyleList *selectors, const char * const name) const
 {
     if ((selectors == nullptr) || (name == nullptr) || strlen(name) == 0) {
         return nullptr;
     }
 
-    AppStyle* first = const_cast<AppStyle *>(selectors->GetFirst());
+    AppStyle *first = const_cast<AppStyle *>(selectors->GetFirst());
     while (first != nullptr) {
         const char *styleName = first->GetStyleName();
         if (styleName != nullptr && !strcmp(name, styleName)) {

openharmony/ace_engine_lite

Signed-off-by: jiadexiang <jiadexiang@huawei.com>

frameworks/src/core/stylemgr/app_style.cpp
@@ -326,5 +326,28 @@ AppStyle *AppStyle::GenerateFromJS(jerry_value_t styleKey, jerry_value_t styleVa
 
     return newStyle;
 }
+
+void AppStyle::CombineStyles(AppStyle &dest, const AppStyle &source, bool overwrite)
+{
+    const AppStyleItem *styleItem = source.GetFirst();
+    while (styleItem != nullptr) {
+        const AppStyleItem *currentStyleItemInSource = styleItem;
+        styleItem = styleItem->GetNext();
+        const AppStyleItem *existOneInDest = dest.GetStyleItemByNameId(currentStyleItemInSource->GetPropNameId());
+
+        if (existOneInDest == nullptr) {
+            dest.AddStyleItem(AppStyleItem::CopyFrom(currentStyleItemInSource));
+            continue;
+        }
+
+        if (!overwrite) {
+            // exist one has higher priority
+            continue;
+        }
+
+        // the new one has higher priority, to over wirte exist one
+        const_cast<AppStyleItem *>(existOneInDest)->UpdateValueFrom(*currentStyleItemInSource);
+    }
+}
 } // namespace ACELite
 } // namespace OHOS

frameworks/src/core/stylemgr/app_style.h
@@ -33,7 +33,7 @@ public:
         Reset();
     }
 
-    const char* GetStyleName()
+    const char* GetStyleName() const
     {
         return styleName_;
     }
@@ -65,7 +65,7 @@ public:
         next_ = nextStyle;
     }
 
-    const AppStyle* GetNext()
+    const AppStyle* GetNext() const
     {
         return next_;
     }
@@ -75,6 +75,7 @@ public:
     const AppStyleItem* GetStyleItemByName(const char * const stylePropName) const;
     const AppStyleItem* GetStyleItemByNameId(uint16_t stylePropNameId) const;
     static AppStyle* GenerateFromJS(jerry_value_t styleKey, jerry_value_t styleValue, bool isKeyFrames);
+    static void CombineStyles(AppStyle &dest, const AppStyle &source, bool overwrite = false);
 
 private:
     void SetStyleName(const char * const name, size_t nameLen);

frameworks/src/core/stylemgr/app_style_sheet.cpp
@@ -14,6 +14,9 @@
  */
 
 #include "stylemgr/app_style_sheet.h"
+#include "condition_arbitrator.h"
+#include "js_fwk_common.h"
+#include "scope_js_value.h"
 
 namespace OHOS {
 namespace ACELite {
@@ -44,15 +47,84 @@ void AppStyleSheet::InitSheet(jerry_value_t styleSheetObj)
         return;
     }
 
+    // clear previous style sheet list
     Reset();
 
+    // initializa all media query selectors first, as all media query selectors have higher priority
+    InitMediaSelectors(styleSheetObj);
+
+    // initialize all other normal selectors
+    InitNormalSelectors(styleSheetObj);
+}
+
+void AppStyleSheet::InitMediaSelectors(const jerry_value_t styleSheetObj)
+{
+    // check if media query exists in style sheet
+    const char * const mediaQueryArrayKeyName = "@media";
+    jerry_value_t propNameV =
+            jerry_create_string(reinterpret_cast<jerry_char_t *>(const_cast<char *>(mediaQueryArrayKeyName)));
+    ScopeJSValue propNameP(propNameV);
+    if (!HasOwnProperty(styleSheetObj, propNameV)) {
+        // no media querry items
+        return;
+    }
+
+    ScopeJSValue mediaQueryArrayValue(jerry_get_property(styleSheetObj, propNameV));
+    if (!jerry_value_is_array(mediaQueryArrayValue.Obain())) {
+        return;
+    }
+
+    uint32_t conditionCounts = jerry_get_array_length(mediaQueryArrayValue.Obain());
+    const uint32_t maxConditionCount = 256;
+    if (conditionCounts == 0 || conditionCounts > maxConditionCount) {
+        return;
+    }
+    for (uint32_t index = 0; index < conditionCounts; index++) {
+        // get the array item @ index position
+        jerry_value_t mediaArrayItemV = jerry_get_property_by_index(mediaQueryArrayValue.Obain(), index);
+        ScopeJSValue mediaArrayItemP(mediaArrayItemV);
+        // handle single one
+        HandleSingleMediaItem(mediaArrayItemV);
+    }
+}
+
+void AppStyleSheet::HandleSingleMediaItem(const jerry_value_t mediaItem)
+{
+    const char * const conditionKeyName = "condition";
+    jerry_value_t conditionKeyV = jerry_create_string(reinterpret_cast<const jerry_char_t *>(conditionKeyName));
+    ScopeJSValue conditionKeyP(conditionKeyV);
+    if (!JerryHasProperty(mediaItem, conditionKeyV)) {
+        return;
+    }
+    // get condition string
+    jerry_value_t conditionStrJSValue = jerry_get_property(mediaItem, conditionKeyV);
+    ScopeJSValue autoReleaseV(conditionStrJSValue);
+    char *conditionStr = MallocStringOf(conditionStrJSValue);
+    if (conditionStr == nullptr) {
+        return;
+    }
+    ConditionArbitrator arbitrator;
+    bool isMatched = arbitrator.Decide(conditionStr);
+    ace_free(conditionStr);
+    conditionStr = nullptr;
+    if (!isMatched) {
+        // the current media item is not matching with current device environment
+        return;
+    }
+
+    // matched, parse the item through normal way
+    InitNormalSelectors(mediaItem, true);
+}
+
+void AppStyleSheet::InitNormalSelectors(const jerry_value_t styleSheetObj, bool overwrite)
+{
     // init all id selectors
     const char * const attrIdSelectors = "idSelectors";
     jerry_value_t propName = jerry_create_string(reinterpret_cast<jerry_char_t *>(const_cast<char *>(attrIdSelectors)));
     jerry_value_t propValue = UNDEFINED;
     if (HasOwnProperty(styleSheetObj, propName)) {
         propValue = jerry_get_property(styleSheetObj, propName);
-        InitSelectors(&idSelectors_, propValue, false);
+        InitSelectors(&idSelectors_, propValue, false, overwrite);
         jerry_release_value(propValue);
     }
     jerry_release_value(propName);
@@ -62,7 +134,7 @@ void AppStyleSheet::InitSheet(jerry_value_t styleSheetObj)
     propName = jerry_create_string(reinterpret_cast<jerry_char_t *>(const_cast<char *>(attrClassSelectors)));
     if (HasOwnProperty(styleSheetObj, propName)) {
         propValue = jerry_get_property(styleSheetObj, propName);
-        InitSelectors(&classSelectors_, propValue, false);
+        InitSelectors(&classSelectors_, propValue, false, overwrite);
         jerry_release_value(propValue);
     }
     jerry_release_value(propName);
@@ -72,13 +144,14 @@ void AppStyleSheet::InitSheet(jerry_value_t styleSheetObj)
     propName = jerry_create_string(reinterpret_cast<jerry_char_t *>(const_cast<char *>(keyFrames)));
     if (HasOwnProperty(styleSheetObj, propName)) {
         propValue = jerry_get_property(styleSheetObj, propName);
-        InitSelectors(&keyFrameSelectors_, propValue, true);
+        InitSelectors(&keyFrameSelectors_, propValue, true, overwrite);
         jerry_release_value(propValue);
     }
     jerry_release_value(propName);
 }
 
-void AppStyleSheet::InitSelectors(AppStyleList** selectorsList, jerry_value_t selectorsObj, bool isKeyFrames)
+void AppStyleSheet::InitSelectors(AppStyleList **selectorsList, jerry_value_t selectorsObj,
+                                  bool isKeyFrames, bool overwrite)
 {
     if (jerry_value_is_undefined(selectorsObj)) {
         return;
@@ -98,9 +171,16 @@ void AppStyleSheet::InitSelectors(AppStyleList** selectorsList, jerry_value_t se
         /* convert style key into char */
         jerry_value_t styleKey = jerry_get_property_by_index(styleKeys, index);
         jerry_value_t styleValue = jerry_get_property(selectorsObj, styleKey);
-        AppStyle* newStyle = AppStyle::GenerateFromJS(styleKey, styleValue, isKeyFrames);
+        AppStyle *newStyle = AppStyle::GenerateFromJS(styleKey, styleValue, isKeyFrames);
         if (newStyle != nullptr) {
-            (*selectorsList)->AddStyle(newStyle);
+            AppStyle *existOne = (*selectorsList)->GetExistStyle(newStyle->GetStyleName());
+            if (existOne != nullptr) {
+                AppStyle::CombineStyles(*existOne, *newStyle, overwrite);
+                delete newStyle; // combined to exist one, no used
+                newStyle = nullptr;
+            } else {
+                (*selectorsList)->AddStyle(newStyle);
+            }
         }
         ReleaseJerryValue(styleKey, styleValue, VA_ARG_END_FLAG);
     }
@@ -108,28 +188,28 @@ void AppStyleSheet::InitSelectors(AppStyleList** selectorsList, jerry_value_t se
     jerry_release_value(styleKeys);
 }
 
-AppStyle* AppStyleSheet::GetStyleFromIDSelectors(const char * const name) const
+AppStyle *AppStyleSheet::GetStyleFromIDSelectors(const char * const name) const
 {
     return GetStyleFromSelectors(idSelectors_, name);
 }
 
-AppStyle* AppStyleSheet::GetStyleFromClassSelectors(const char * const name) const
+AppStyle *AppStyleSheet::GetStyleFromClassSelectors(const char * const name) const
 {
     return GetStyleFromSelectors(classSelectors_, name);
 }
 
-AppStyle* AppStyleSheet::GetStyleFromKeyFramesSelectors(const char * const name) const
+AppStyle *AppStyleSheet::GetStyleFromKeyFramesSelectors(const char * const name) const
 {
     return GetStyleFromSelectors(keyFrameSelectors_, name);
 }
 
-AppStyle* AppStyleSheet::GetStyleFromSelectors(AppStyleList* selectors, const char * const name) const
+AppStyle *AppStyleSheet::GetStyleFromSelectors(AppStyleList *selectors, const char * const name) const
 {
     if ((selectors == nullptr) || (name == nullptr) || strlen(name) == 0) {
         return nullptr;
     }
 
-    AppStyle* first = const_cast<AppStyle *>(selectors->GetFirst());
+    AppStyle *first = const_cast<AppStyle *>(selectors->GetFirst());
     while (first != nullptr) {
         const char *styleName = first->GetStyleName();
         if (styleName != nullptr && !strcmp(name, styleName)) {