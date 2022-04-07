a84f4475dee647cffd72e2d2ccb730aa3b1066a6
yudechen <chenyude@huawei.com>
2022-04-07 15:46:12 +0800

    add caniuse js api for small system.
    Signed-off-by: yudechen <chenyude@huawei.com>
    Change-Id: I89e93285a51e8bdbc8ae396f94ae0fa520bdd37a

--- frameworks/targets/liteos_m/acelite_config.h
+++ frameworks/targets/liteos_m/acelite_config.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2020-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -72,6 +72,13 @@
  */
 #ifndef FEATURE_FEATURE_ABILITY_MODULE
 #define FEATURE_FEATURE_ABILITY_MODULE 1
+#endif
+
+/**
+ * enable syscap JS API
+ */
+#ifndef FEATURE_SYSCAP_MODULE
+#define FEATURE_SYSCAP_MODULE 0
 #endif
 
 /**
