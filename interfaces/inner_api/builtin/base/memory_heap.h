a14124a8a8ed20c00a544765a99eaf52fbba6c91
suwenxiang <suwenxiang@huawei.com>
2022-04-22 09:40:22 +0800

    Description: Modify the path of ace_engine_lite, ace_engine, napi warehouse names IssueNo: https://gitee.com/openharmony/ace_engine_lite/issues/I53IBC Feature or Bugfix: Bugfix Binary Source:No Signed-off-by: suwenxiang <suwenxiang@huawei.com>

--- interfaces/inner_api/builtin/base/memory_heap.h
+++ interfaces/inner_api/builtin/base/memory_heap.h
@@ -15,7 +15,10 @@
 
 #ifndef OHOS_ACELITE_MEMORY_HEAP_H
 #define OHOS_ACELITE_MEMORY_HEAP_H
+#include <cstddef>
 #include <cstdio>
+
+#include "ace_mem_base.h"
 
 namespace OHOS {
 namespace ACELite {
